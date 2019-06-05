2286ee8f9e0436d8984bae972cc6b15f30b83adb
ferjpi <fernandojpt1203@gmail.com>
2019-06-05 17:57:47 -0500

    Se arreglo la instacia de personas para el vuelo

--- app/controllers/vuelos_controller.rb
+++ app/controllers/vuelos_controller.rb
@@ -1,16 +1,20 @@
 class VuelosController < ApplicationController
+  before_action :set_passsanger, only: [:show, :index]
   def index
       @vuelo = Vuelo.search(vuelo_params)
-  end
-
-  def show
-    @vuelo = Vuelo.find_by(id: params[:id])
-    @passenger = params[:passenger]
-  end
-
-  private
-
+    end
+    
+    def show
+      @vuelo = Vuelo.find_by(id: params[:id])
+    end
+    
+    private
+    
     def vuelo_params
-      params.permit(:departure, :destination, :passenger)
+      params.permit(:departure, :destination, :passenger, :people)
+    end
+    
+    def set_passsanger
+      @people = params
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_230534) do
+ActiveRecord::Schema.define(version: 2019_06_05_221001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_230534) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "people"
   end
 
 end
